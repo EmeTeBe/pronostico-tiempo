05e59c6ebff76911fa7b33e7fa5ea9c8e11ae726
EmeTeBe <bernimarozzi@gmail.com>
2025-12-04 18:19:03 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import styles from "./App.module.css";
+import Form from "./components/Form/Form";
+import WeatherDetail from "./components/WeatherDetail/WeatherDetail";
+import useWeather from "./hooks/useWeather";
+
+function App() {
+  const { weather, fetchWeather, hasWeatherData } = useWeather();
+
+  return (
+    <>
+      <h1 className={styles.title}>Buscador Pronostico del tiempo</h1>
+      <div className={styles.container}>
+        <Form fetchWeather={fetchWeather} />
+        {hasWeatherData && <WeatherDetail weather={weather} />}
+      </div>
+    </>
+  );
+}
+
+export default App;
