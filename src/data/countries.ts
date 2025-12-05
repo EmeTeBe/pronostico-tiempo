2dba2e67cd1c651e5b22f31ca4fe6b404589bb47
EmeTeBe <bernimarozzi@gmail.com>
2025-12-05 12:00:26 -0300

    algunos países más

--- src/data/countries.ts
+++ src/data/countries.ts
@@ -1,10 +1,15 @@
 import type { Country } from "../types";
 
 export const countries: Country[] = [
-  { code: "MX", name: "México" },
   { code: "AR", name: "Argentina" },
+  { code: "PY", name: "Paraguay" },
+  { code: "MX", name: "México" },
+  { code: "BR", name: "Brasil" },
   { code: "CO", name: "Colombia" },
   { code: "CR", name: "Costa Rica" },
-  { code: "ES", name: "España" },
+  { code: "CU", name: "Cuba" },
+  { code: "EC", name: "Ecuador" },
+  { code: "BO", name: "Bolivia" },
   { code: "PE", name: "Perú" },
+  { code: "CN", name: "China" },
 ];
